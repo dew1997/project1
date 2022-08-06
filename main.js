9f9d5dcb587d9679b68b6a8594d8df2086d722f5
Tan De Wei <dewei@Tans-MacBook-Air.local>
2022-08-06 15:43:14 +0800

    created grid for the gameboard

--- main.js
+++ main.js
@@ -1,23 +1,71 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import $ from "jquery";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//// Battleship game
+/// need different types of battleship
+/// need a function for hit button to fire torpedo,
+/// need a function for sunk battleship
+///
+/*----- constants -----*/
+const rows = 10;
+const cols = 10;
+// Fixed 10x10 grid for the boardsize
+const squareSize = 40;
+// Size of the grid
 
-setupCounter(document.querySelector('#counter'))
+let gameBoard = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+// /*----- functions -----*/
+
+const $gameBoardContainer = $("#gameboard");
+// creating the gameboard
+const createBoardSize = (i, j) => {
+  for (i = 0; i < cols; i++) {
+    for (j = 0; j < rows; j++) {
+      const $square = $("<div>")
+        .attr("id", "s" + j + i)
+        .addClass("squares");
+      let topPosition = j * squareSize;
+      let leftPosition = i * squareSize;
+      $square.css("top", topPosition + "px");
+      $square.css("left", leftPosition + "px");
+      $("#gameboard").append($square);
+    }
+  }
+};
+
+// bring you to game page
+const startGame = () => {
+  const $button = $(".button");
+  $button.on("click", () => {
+    const $description = $("p").show();
+    $button.hide();
+  });
+};
+
+const main = () => {
+  const $description = $("p").hide();
+  startGame();
+  createBoardSize();
+};
+
+main();
+
+$(".squares").on("click", (event) => {
+  console.log(event);
+});
+
+$description.hide();
