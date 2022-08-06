79b5a15610bf97671c01698ed0da5b94b79db635
Tan De Wei <dewei@Tans-MacBook-Air.local>
2022-08-06 17:02:12 +0800

    fixed rendering

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import $ from "jquery";
 /// need a function for hit button to fire torpedo,
 /// need a function for sunk battleship
 ///
+const game = {
+  page: "#start",
+};
 /*----- constants -----*/
 const rows = 10;
 const cols = 10;
@@ -30,7 +33,7 @@ let gameBoard = [
 /*----- event listeners -----*/
 // /*----- functions -----*/
 
-const $gameBoardContainer = $("#gameboard");
+const $gameBoardGrid = $("#gameboard");
 // creating the gameboard
 const createBoardSize = (i, j) => {
   for (i = 0; i < cols; i++) {
@@ -42,30 +45,28 @@ const createBoardSize = (i, j) => {
       let leftPosition = i * squareSize;
       $square.css("top", topPosition + "px");
       $square.css("left", leftPosition + "px");
-      $("#gameboard").append($square);
+      $gameBoardGrid.append($square);
     }
   }
 };
-
-// bring you to game page
-const startGame = () => {
-  const $button = $(".button");
-  $button.on("click", () => {
-    const $description = $("p").show();
-    $button.hide();
-  });
-};
-
-const main = () => {
-  const $description = $("p").hide();
-  startGame();
-  createBoardSize();
-};
-
-main();
 
 $(".squares").on("click", (event) => {
   console.log(event);
 });
 
-$description.hide();
+const render = () => {
+  $(".page").hide();
+  $(game.page).show();
+
+  createBoardSize();
+};
+
+const main = () => {
+  $(".button").on("click", () => {
+    game.page = "#game";
+    render();
+  });
+  render();
+};
+
+main();
