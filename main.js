1309fe1ec113846f3346558dfb833916bae9c4b7
Tan De Wei <dewei@Tans-MacBook-Air.local>
2022-08-07 14:20:45 +0800

    update

--- main.js
+++ main.js
@@ -2,38 +2,77 @@ import $ from "jquery";
 
 //// Battleship game
 /// need different types of battleship
+/// considering 5 different battle ship sizes,
+/// 1,2,3,4,5 blocks
 /// need a function for hit button to fire torpedo,
+///
 /// need a function for sunk battleship
 ///
+/*----- constants -----*/
 const game = {
   page: "#start",
+  hitcount: 0,
+  miss: 0,
+  battleship: 17,
+  torpedos: 30,
+
+  board: [
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ],
 };
-/*----- constants -----*/
+//
 const rows = 10;
 const cols = 10;
-// Fixed 10x10 grid for the boardsize
 const squareSize = 40;
+const $gameBoardGrid = $("#gameboard");
+//
+//// Fixed 10x10 grid for the boardsize
 // Size of the grid
-
-let gameBoard = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-/*----- app's state (variables) -----*/
-/*----- cached element references -----*/
-
-/*----- event listeners -----*/
+//
 // /*----- functions -----*/
 
-const $gameBoardGrid = $("#gameboard");
+// event targetting on click
+
+///
+$gameBoardGrid.on("click", (event) => {
+  fireTorpedo(event);
+});
+
+const fireTorpedo = (event) => {
+  if (event.target !== event.currentTarget) {
+    let row = event.target.id.substring(1, 2);
+    let col = event.target.id.substring(2, 3);
+    console.log(row);
+    console.log(col);
+
+    if (game.board[row][col] === 0) {
+      event.target.style.background = "white";
+      game.board[row][col] = 3;
+      game.miss++;
+      if (game.miss == game.torpedos) {
+        alert("You have used up all your torpedos, you lose!");
+      }
+    } else if (game.board[row][col] === 1) {
+      event.target.style.background = "red";
+      game.board[row][col] = 2;
+      game.hitcount++;
+      if (game.hitcount === game.battleship) {
+        alert("Congratulations, you have destroyed all the battleship!");
+      }
+    } else if (game.board[row][col] > 1) {
+      alert("Spot already fired!");
+    }
+  }
+};
 // creating the gameboard
 const createBoardSize = (i, j) => {
   for (i = 0; i < cols; i++) {
@@ -49,11 +88,7 @@ const createBoardSize = (i, j) => {
     }
   }
 };
-
-$(".squares").on("click", (event) => {
-  console.log(event);
-});
-
+///
 const render = () => {
   $(".page").hide();
   $(game.page).show();
